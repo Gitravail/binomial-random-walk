6e3ead8ef824b66bfc71716be9175a4a0a887468
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-01-12 21:36:36 +0100

    feat: remove zeros on outer sides

--- main.py
+++ main.py
@@ -2,35 +2,45 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-INNER_ITERATION = 50
+INNER_ITERATION = 100
 MAX_ITERATION = 100000
 
 
-def compute_result(non_computed_result):
-    computed_result = [0] * (INNER_ITERATION * 2 + 1)
-    for current in non_computed_result:
-        computed_result[current + INNER_ITERATION] += 1
-    return computed_result
+def compute_result(random_heights):
+    heights = [0] * (INNER_ITERATION * 2 + 1)
+    for result in random_heights:
+        heights[result + INNER_ITERATION] += 1
+    return heights
 
 
-def generate_axes_array():
-    result = []
+def generate_axes_array(heights):
+    xvalues = []
     for i in range(-INNER_ITERATION, INNER_ITERATION + 1):
-        result.append(i)
-    return result
+        xvalues.append(i)
+    return xvalues
 
 
-def display_result(computed_result):
-    possible_values = generate_axes_array()
-    results = computed_result
-    y_pos = np.arange(len(possible_values))
-    plt.xticks(y_pos, possible_values, rotation=90, fontsize=5)
-    plt.bar(y_pos, results)
+def remove_out_zeros(values, heights):
+    while len(heights) and heights[0] == 0:
+        values.pop(0)
+        heights.pop(0)
+
+    while len(heights) and heights[len(heights)-1] == 0:
+        values.pop()
+        heights.pop()
+
+
+def display_result(heights):
+    xvalues = generate_axes_array(heights)
+    remove_out_zeros(xvalues, heights)
+    y_pos = np.arange(len(xvalues))
+    plt.xticks(y_pos, xvalues, rotation=90, fontsize=5)
+    plt.bar(y_pos, heights)
     plt.show()
 
 
 def main():
-    result = []
+    fn = []
     for i in range(MAX_ITERATION):
         k = 0
         for j in range(INNER_ITERATION):
@@ -39,8 +49,8 @@ def main():
             else:
                 k -= 1
             j += 1
-        result.append(k)
-    display_result(compute_result(result))
+        fn.append(k)
+    display_result(compute_result(fn))
 
 
 if __name__ == '__main__':
