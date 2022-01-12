10717d5a0d558eb37e829ca14307f63bcbf6b5b7
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-01-12 23:23:44 +0100

    feat: add tkinter integration

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import random
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
+from tkinter import *
 
-INNER_ITERATION = 100
-MAX_ITERATION = 100000
+INNER_ITERATION = 10
+MAX_ITERATION = 10000
+
+window = Tk()
 
 
 def compute_result(random_heights):
@@ -32,16 +37,33 @@ def remove_out_zeros(values_with_zeros, heights_with_zeros):
     return values, heights
 
 
-def display_result(heights):
+def init_window():
+    window.title("Binomial random walk")
+    window.geometry("1280x720")
+    compute_button = Button(command=get_figure, master=window, height=2, width=10, text="Plot")
+    compute_button.pack()
+    window.mainloop()
+
+
+def get_figure():
+    heights = compute_result(compute_all())
     xvalues, heights = remove_out_zeros(generate_axes_array(heights), heights)
     y_pos = np.arange(len(xvalues))
-    plt.xticks(y_pos, xvalues, rotation=90, fontsize=5)
+    fig = Figure(figsize=(10, 6), dpi=100)
+    plt = fig.add_subplot(111)
+    plt.set_xticks(y_pos, minor=False)
+    plt.set_xticklabels(xvalues, fontdict=None, minor=False)
     plt.bar(y_pos, heights)
     plt.plot(y_pos, heights, color='red')
-    plt.show()
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+    canvas.get_tk_widget().pack()
 
 
-def main():
+def compute_all():
     fn = []
     for i in range(MAX_ITERATION):
         k = 0
@@ -52,7 +74,11 @@ def main():
                 k -= 1
             j += 1
         fn.append(k)
-    display_result(compute_result(fn))
+    return fn
+
+
+def main():
+    init_window()
 
 
 if __name__ == '__main__':
