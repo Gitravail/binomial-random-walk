c1d2c176c17cc6a41e1b005c65c34c9f51fb7f58
Raphaël Tournafond <raphael.tournafond@etu.univ-smb.fr>
2022-01-18 16:44:37 +0100

    refactor: avoid long line

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class App(Frame):
     button_frame = Frame(right_frame)
     inner = Scale(slider_frame, from_=10, to=100, orient=HORIZONTAL)
     outer = Scale(slider_frame, from_=1000, to=100000, resolution=1000, orient=HORIZONTAL)
-    dimension_check = Checkbutton(button_frame, text="2D", variable=dimension, onvalue=1, offvalue=0, width=20, height=5)
+    dimension_check = Checkbutton(button_frame, text="2D", variable=dimension, onvalue=1,
+                                  offvalue=0, width=20, height=5)
 
     def __init__(self):
         super().__init__()
